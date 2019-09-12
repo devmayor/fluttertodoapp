d58b8c26c98017827c61ef65644a8b0b87002632
Afolabi Mayowa <afotrick2011@gmail.com>
2019-09-12 02:49:38 +0100

    update

--- lib/test.dart
+++ lib/test.dart
@@ -1,130 +1,130 @@
-// Expanded(
-//               child: 
-//               Container(
-//                 padding: EdgeInsets.symmetric(vertical: 30, horizontal: 45),
-//                 width: double.infinity,
-//                 height: double.infinity,
-//                 margin: EdgeInsets.only(top: 30),
-//                 // constraints: BoxConstraints.expand(),
-//                 decoration: BoxDecoration(
-//                     color: Colors.white,
-//                     borderRadius: BorderRadius.only(
-//                         topLeft: Radius.elliptical(25, 25),
-//                         topRight: Radius.elliptical(25, 25))),
-//                   // child: Column(
-//                   // crossAxisAlignment: CrossAxisAlignment.start,
-//                   // children: <Widget>[
-//                   //   Container(
-//                   //     child: Expanded(
-//                   //       child: Column(
-//                   //         crossAxisAlignment: CrossAxisAlignment.start,
-//                   //         children: <Widget>[
-//                   //           Text(
-//                   //             "Late",
-//                   //             style: TextStyle(color: widget.textColor),
-//                   //           ),
-//                   //           Expanded(
-//                   //             child: ListView(
-//                   //               children: <Widget>[
-//                   //                 CheckboxListTile(
-//                   //                   title: Text("Call Max"),
-//                   //                   subtitle: Text("2015 April 29"),
-//                   //                   value: false,
-//                   //                   onChanged: (val) => print(
-//                   //                       "this is pressed " + val.toString()),
-//                   //                 ),
-//                   //                 CheckboxListTile(
-//                   //                   title: Text("Call Max"),
-//                   //                   subtitle: Text("2015 April 29"),
-//                   //                   value: false,
-//                   //                   onChanged: (val) => print(
-//                   //                       "this is pressed " + val.toString()),
-//                   //                 ),
-//                   //                 CheckboxListTile(
-//                   //                   title: Text("Call Max"),
-//                   //                   subtitle: Text("2015 April 29"),
-//                   //                   value: false,
-//                   //                   onChanged: (val) => print(
-//                   //                       "this is pressed " + val.toString()),
-//                   //                 ),
-//                   //                 CheckboxListTile(
-//                   //                   title: Text("Call Max"),
-//                   //                   subtitle: Text("2015 April 29"),
-//                   //                   value: false,
-//                   //                   onChanged: (val) => print(
-//                   //                       "this is pressed " + val.toString()),
-//                   //                 ),
-//                   //                 CheckboxListTile(
-//                   //                   title: Text("Call Max"),
-//                   //                   subtitle: Text("2015 April 29"),
-//                   //                   value: false,
-//                   //                   onChanged: (val) => print(
-//                   //                       "this is pressed " + val.toString()),
-//                   //                 ),
-//                   //               ],
-//                   //             ),
-//                   //           )
-//                   //         ],
-//                   //       ),
-//                   //     ),
-//                   //   ),
-//                   //   Container(
-//                   //     child: Expanded(
-//                   //       child: Column(
-//                   //         crossAxisAlignment: CrossAxisAlignment.start,
-//                   //         children: <Widget>[
-//                   //           Text(
-//                   //             "Today",
-//                   //             style: TextStyle(color: widget.textColor),
-//                   //           ),
-//                   //           Expanded(
-//                   //             child: ListView(
-//                   //               children: <Widget>[
-//                   //                 CheckboxListTile(
-//                   //                   title: Text("Call Max"),
-//                   //                   subtitle: Text("2015 April 29"),
-//                   //                   value: false,
-//                   //                   onChanged: (val) => print(
-//                   //                       "this is pressed " + val.toString()),
-//                   //                 ),
-//                   //                 CheckboxListTile(
-//                   //                   title: Text("Call Max"),
-//                   //                   subtitle: Text("2015 April 29"),
-//                   //                   value: false,
-//                   //                   onChanged: (val) => print(
-//                   //                       "this is pressed " + val.toString()),
-//                   //                 ),
-//                   //                 CheckboxListTile(
-//                   //                   title: Text("Call Max"),
-//                   //                   subtitle: Text("2015 April 29"),
-//                   //                   value: false,
-//                   //                   onChanged: (val) => print(
-//                   //                       "this is pressed " + val.toString()),
-//                   //                 ),
-//                   //                 CheckboxListTile(
-//                   //                   title: Text("Call Max"),
-//                   //                   subtitle: Text("2015 April 29"),
-//                   //                   value: false,
-//                   //                   onChanged: (val) => print(
-//                   //                       "this is pressed " + val.toString()),
-//                   //                 ),
-//                   //                 CheckboxListTile(
-//                   //                   title: Text("Call Max"),
-//                   //                   subtitle: Text("2015 April 29"),
-//                   //                   value: false,
-//                   //                   onChanged: (val) => print(
-//                   //                       "this is pressed " + val.toString()),
-//                   //                 ),
-//                   //               ],
-//                   //             ),
-//                   //           )
-//                   //         ],
-//                   //       ),
-//                   //     ),
-//                   //   ),
-//                   // ],
+Expanded(
+              child: 
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 45),
+                width: double.infinity,
+                height: double.infinity,
+                margin: EdgeInsets.only(top: 30),
+                // constraints: BoxConstraints.expand(),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.elliptical(25, 25),
+                        topRight: Radius.elliptical(25, 25))),
+                  // child: Column(
+                  // crossAxisAlignment: CrossAxisAlignment.start,
+                  // children: <Widget>[
+                  //   Container(
+                  //     child: Expanded(
+                  //       child: Column(
+                  //         crossAxisAlignment: CrossAxisAlignment.start,
+                  //         children: <Widget>[
+                  //           Text(
+                  //             "Late",
+                  //             style: TextStyle(color: widget.textColor),
+                  //           ),
+                  //           Expanded(
+                  //             child: ListView(
+                  //               children: <Widget>[
+                  //                 CheckboxListTile(
+                  //                   title: Text("Call Max"),
+                  //                   subtitle: Text("2015 April 29"),
+                  //                   value: false,
+                  //                   onChanged: (val) => print(
+                  //                       "this is pressed " + val.toString()),
+                  //                 ),
+                  //                 CheckboxListTile(
+                  //                   title: Text("Call Max"),
+                  //                   subtitle: Text("2015 April 29"),
+                  //                   value: false,
+                  //                   onChanged: (val) => print(
+                  //                       "this is pressed " + val.toString()),
+                  //                 ),
+                  //                 CheckboxListTile(
+                  //                   title: Text("Call Max"),
+                  //                   subtitle: Text("2015 April 29"),
+                  //                   value: false,
+                  //                   onChanged: (val) => print(
+                  //                       "this is pressed " + val.toString()),
+                  //                 ),
+                  //                 CheckboxListTile(
+                  //                   title: Text("Call Max"),
+                  //                   subtitle: Text("2015 April 29"),
+                  //                   value: false,
+                  //                   onChanged: (val) => print(
+                  //                       "this is pressed " + val.toString()),
+                  //                 ),
+                  //                 CheckboxListTile(
+                  //                   title: Text("Call Max"),
+                  //                   subtitle: Text("2015 April 29"),
+                  //                   value: false,
+                  //                   onChanged: (val) => print(
+                  //                       "this is pressed " + val.toString()),
+                  //                 ),
+                  //               ],
+                  //             ),
+                  //           )
+                  //         ],
+                  //       ),
+                  //     ),
+                  //   ),
+                  //   Container(
+                  //     child: Expanded(
+                  //       child: Column(
+                  //         crossAxisAlignment: CrossAxisAlignment.start,
+                  //         children: <Widget>[
+                  //           Text(
+                  //             "Today",
+                  //             style: TextStyle(color: widget.textColor),
+                  //           ),
+                  //           Expanded(
+                  //             child: ListView(
+                  //               children: <Widget>[
+                  //                 CheckboxListTile(
+                  //                   title: Text("Call Max"),
+                  //                   subtitle: Text("2015 April 29"),
+                  //                   value: false,
+                  //                   onChanged: (val) => print(
+                  //                       "this is pressed " + val.toString()),
+                  //                 ),
+                  //                 CheckboxListTile(
+                  //                   title: Text("Call Max"),
+                  //                   subtitle: Text("2015 April 29"),
+                  //                   value: false,
+                  //                   onChanged: (val) => print(
+                  //                       "this is pressed " + val.toString()),
+                  //                 ),
+                  //                 CheckboxListTile(
+                  //                   title: Text("Call Max"),
+                  //                   subtitle: Text("2015 April 29"),
+                  //                   value: false,
+                  //                   onChanged: (val) => print(
+                  //                       "this is pressed " + val.toString()),
+                  //                 ),
+                  //                 CheckboxListTile(
+                  //                   title: Text("Call Max"),
+                  //                   subtitle: Text("2015 April 29"),
+                  //                   value: false,
+                  //                   onChanged: (val) => print(
+                  //                       "this is pressed " + val.toString()),
+                  //                 ),
+                  //                 CheckboxListTile(
+                  //                   title: Text("Call Max"),
+                  //                   subtitle: Text("2015 April 29"),
+                  //                   value: false,
+                  //                   onChanged: (val) => print(
+                  //                       "this is pressed " + val.toString()),
+                  //                 ),
+                  //               ],
+                  //             ),
+                  //           )
+                  //         ],
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ],
                 
-//                 // ),
-//               ),
-//             )
+                // ),
+              ),
+            )
          
